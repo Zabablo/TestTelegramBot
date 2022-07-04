576a175a92f339a9d76ca2ea1aabdf404976e064
Anton <Zabablo7@gmail.com>
2022-07-04 18:31:06 +0400

    mybe firstCommit

--- main.py
+++ main.py
@@ -2,13 +2,21 @@ import telebot
 
 bot = telebot.TeleBot('5469747449:AAE-AmaMbddvMIWZSq9HxthW7GZWGTXrHhA')
 
+
 @bot.message_handler(commands=['start'])
 def start(message):
     mess = f'Привет, <b>{message.from_user.first_name} <u>{message.from_user.last_name}</u></b>'
     bot.send_message(message.chat.id, mess, parse_mode='html')
 
+
 @bot.message_handler()
 def get_user_text(message):
-    bot.send_message()
+    if message.text == 'Hello':
+        bot.send_message(message.chat.id, "И тебе привет", parse_mode='html')
+    elif message.text == 'id':
+        bot.send_message(message.chat.id, f"Твой ID: {message.from_user.id}", parse_mode='html')
+    elif message.text == 'id':
+        bot.send_message(message.chat.id, "Я тебя не понимаю", parse_mode='html')
+
 
 bot.polling(none_stop=True)
