b90b5027f6dcc1951b107ba01bf71cdf2af8d5e6
Anton <Zabablo7@gmail.com>
2022-07-06 19:08:25 +0400

    create get_user_photo

--- main.py
+++ main.py
@@ -9,14 +9,22 @@ def start(message):
     bot.send_message(message.chat.id, mess, parse_mode='html')
 
 
-@bot.message_handler()
+@bot.message_handler(content_types=['text'])
 def get_user_text(message):
     if message.text == 'Hello':
         bot.send_message(message.chat.id, "И тебе привет", parse_mode='html')
-    elif message.text == 'id':
+    elif message.text == 'Id':
         bot.send_message(message.chat.id, f"Твой ID: {message.from_user.id}", parse_mode='html')
-    elif message.text == 'id':
+    elif message.text == 'Meme':
+        photo = open('mem1.jpg', 'rb')
+        bot.send_photo(message.chat.id, photo)
+    else:
         bot.send_message(message.chat.id, "Я тебя не понимаю", parse_mode='html')
+
+
+@bot.message_handler(content_types=['photo'])
+def get_user_photo(message):
+    bot.send_message(message.chat.id, 'Вау, крутое фото')
 
 
 bot.polling(none_stop=True)
